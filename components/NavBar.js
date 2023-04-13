b76c6222cd0cecd6c62c5f39e5140420822ca196
flsnrtmmyungju <mjmj01617@naver.com>
2023-04-13 09:48:14 +0900

    css적용: 모듈파일만들고 className={모듈.스타일이름}

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import { useRouter } from "next/router"
-
+import styles from "./NavBar.module.css"
 
 export default function NavBar() {
     const router = useRouter()
-    return <nav>
+    return <nav className={styles.nav}>
         {/* 추천하진 않지만 style 사용하는 방법 */}
         {/* <Link href="/" style={{ color: router.pathname === "/" ? 'red' : 'blue' }}>home</Link> */}
         <Link href="/" style={{ color: router.pathname === "/" ? 'red' : 'blue' }}>home</Link>
