0459dfe56c40aaa883d1cc03175b345e922992b5
flsnrtmmyungju <mjmj01617@naver.com>
2023-04-13 09:38:34 +0900

    추천하진 않지만 next에서 스타일 사용하는 방법 {{}} 두개사용해야하는듯듯

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,7 +5,9 @@ import { useRouter } from "next/router"
 export default function NavBar() {
     const router = useRouter()
     return <nav>
-        <Link href="/">home</Link>
-        <Link href="/about">about</Link>
+        {/* 추천하진 않지만 style 사용하는 방법 */}
+        {/* <Link href="/" style={{ color: router.pathname === "/" ? 'red' : 'blue' }}>home</Link> */}
+        <Link href="/" style={{ color: router.pathname === "/" ? 'red' : 'blue' }}>home</Link>
+        <Link href="/about" style={{ color: router.pathname === "/" ? 'blue' : 'red' }}>about</Link>
     </nav>
 }
