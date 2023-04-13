4a6e2b4cbd83634af73e0b81a847df3ca4072c87
flsnrtmmyungju <mjmj01617@naver.com>
2023-04-13 10:15:26 +0900

    css모듈 사용하지않고 스타일링하는 방법

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,13 +1,21 @@
 import Link from "next/link";
 import { useRouter } from "next/router"
-import styles from "./NavBar.module.css"
 
 export default function NavBar() {
     const router = useRouter()
     return <nav >
         {/* 추천하진 않지만 style 사용하는 방법 */}
         {/* <Link href="/" style={{ color: router.pathname === "/" ? 'red' : 'blue' }}>home</Link> */}
-        <Link href="/" className={router.pathname === "/" ? styles.active : ""}>home</Link>
-        <Link href="/about" className={router.pathname === "/about" ? styles.active : ""}>about</Link>
+        <Link href="/" ><span className={router.pathname === "/" ? "active" : '' }  >home</span></Link>
+        <Link href="/about">about</Link>
+        <style jsx>{`
+            nav{ background-color : tomato;
+            }        
+            .active{
+                text-decoration: none;
+                color:yellow;
+            }
+        
+         `}</style>
     </nav>
 }
